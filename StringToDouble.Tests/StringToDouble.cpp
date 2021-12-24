b6930b96ca822c79c9fcbc969149c0ad81a14e0d
Карабейников Данила Андреевич <93384239+danilaKarabeynikov@users.noreply.github.com>
2021-12-24 13:20:39 +0300

    initial

---
+++ StringToDouble.Tests/StringToDouble.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "../Let4Karabejnikov/StringToDouble.hpp"
+
+TEST(StringToDoubleTests, Test1)
+{
+	char str[] = "-64";
+	ASSERT_NEAR(StringToDouble(str, 8), -52, 0.0000001);
+}
+
+TEST(StringToDoubleTests, Test2)
+{
+	char str[] = "-b2a.a";
+	ASSERT_NEAR(StringToDouble(str, 13), -1895.769230, 0.0001);
+}
